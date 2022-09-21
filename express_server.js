6bda0f4170bf2889cd94b69719a0920d29975c9f
DionKanhai <dionkanhais@gmail.com>
2022-09-21 00:39:33 +0000

    update server to handle requests from client for logging in and logging out of webpage

--- express_server.js
+++ express_server.js
@@ -1,17 +1,8 @@
-// Import express library 
+// REQUIREMENTS
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080;
-
-const cookieParser = require('cookie-parser')
-
-// set ejs as the view engine
-app.set('view engine', 'ejs');
-// convert the request body from a buffer into a string we can read
-app.use(express.urlencoded({ extended: true }));
-
-//this is a middleware for cookies
-app.use(cookieParser())
 
 // function that generates a string of 6 random alphanumeric characters
 const generateRandomString = function(stringLength = 6) {
@@ -29,35 +20,54 @@ const urlDatabase = {
   '9sm5xK': "http://google.com"
 };
 
-// define the route that will match POST request and handle it
+// SETTING MIDDLEWARES
+
+// set ejs as the view engine
+app.set('view engine', 'ejs');
+// convert the request body from a buffer into a object we can read
+app.use(express.urlencoded({ extended: true }));
+//this is a middleware for cookies
+app.use(cookieParser())
+
+// ROUTES/ENDPOINTS
+//URLS CRUD API 
+// define the route that will match POST request and handle it (create)
 app.post('/urls', (req, res) => {
   const shortIdForLongUrl = generateRandomString()
   urlDatabase[shortIdForLongUrl] = req.body.longURL
   res.redirect(`/urls/${shortIdForLongUrl}`);
 });
 
-// post route for deleting short urls
+// set up handler for urlDatabase object (read all)
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+//use the shortURL to redirect to the longURL (read one)
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+// post rerouting edit button (update)
+app.post('/urls/:id', (req, res) =>  {
+  const shortIdForLongUrl = req.params.id
+  urlDatabase[shortIdForLongUrl] = req.body.longURL
+  res.redirect('/urls');
+});
+
+// post route for deleting short urls (delete)
 app.post('/urls/:id/delete', (req, res) =>  {
   const shortURL = req.params.id 
   delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
 
-// post route for setting cookie of username in client
-app.post('/urls/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
-});
+// INDEX / RENDERING ROUTES (views)
 
-// post rerouting edit button
-app.post('/urls/:id', (req, res) =>  {
-  res.redirect('/urls');
-});
-
-//use the shortURL to redirect to the longURL
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+// set up handler on root path '/'
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
 // route handler for object with shortened urls
@@ -77,7 +87,7 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new', templateVars);
 });
 
-// client input 
+// url individual detail page
 app.get('/urls/:id', (req, res) => {
   const urlId = req.params.id;
   const templateVars = { 
@@ -88,16 +98,21 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-// set up handler on root path '/'
-app.get('/', (req, res) => {
-  res.send('Hello!');
+// AUTHENTICATION API ROUTES
+
+// post route for setting cookie of username in client
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
 });
 
-// set up handler for urlDatabase object
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+// clear cookies and redirect back to home page
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
+// LISTENER FOR INCOMING REQUESTS
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
